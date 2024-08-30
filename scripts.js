29c68ff433ecd33c7671c71b159c96b11c4fa108
jayzhu-dev <jayzhu.dev@gmail.com>
2024-08-30 14:02:20 +1000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,7 +16,7 @@ const projects = {
   ],
   group4: [
     { name: "项目4-1 Blackjack", description: "这是一个用JavaScript实现的项目", url: "project4-1/index.html" },
-    { name: "项目4-2", description: "这是另一个用JavaScript实现的项目", url: "project4-2/index.html" },
+    { name: "项目4-2 Rock Paper Scissors", description: "这是另一个用JavaScript实现的项目", url: "project4-2/index.html" },
     // 添加更多项目...
   ],
   group5: [
